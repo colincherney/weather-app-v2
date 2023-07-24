172dfc4423f880586a86e228e54c0e4ffcc4a0ee
colin11702 <colinmcherney@gmail.com>
2023-07-23 20:18:58 -0700

    Added search input for api

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ window.onresize = function () {
 if ($(window).width() < 1024) {
   $("#myVideo").attr("src", "mobileBg.mov");
 }
+
+function getWeather(location) {
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=f2c31ee9458142fbbd5231344232207&q=" +
+      location +
+      "&aqi=no"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      console.log(response.current.temp_f);
+    })
+    .catch(function (err) {
+      console.log("error: " + err);
+    });
+}
+
+$("#go").on("click", function () {
+  let location = $("input").val();
+  getWeather(location);
+});
