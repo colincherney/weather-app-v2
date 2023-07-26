87c021d02cc28432622c5fd3a75f7191017844be
colin11702 <colinmcherney@gmail.com>
2023-07-26 02:22:26 -0700

    Started setting up cities tab

---
+++ cities.js
@@ -0,0 +1,38 @@
+let cities = [
+  "New York City, United States",
+  "Paris, France",
+  "London, United Kingdom",
+  "Rome, Italy",
+  "Jerusalem, Israel",
+  "Bangkok, Thailand",
+  "Prague, Czech Republic",
+  "Berlin, Germany",
+  "Amsterdam, Netherlands",
+  "Madrid, Spain",
+  "Munich, Germany",
+  "Vienna, Austria",
+  "Brussels, Belgium",
+  "Sydney, Australia",
+  "Rio de Janeiro, Brazil",
+  "Sydney, Australia",
+];
+
+function showCities() {
+  $(".search-and-weather").empty();
+
+  $(".search-and-weather").append(
+    "<div class='search'><input type='text' id='input' placeholder='Search Location' /> </div>"
+  );
+  for (let i = 0; i < 16; i++) {
+    $(".search-and-weather").append(
+      "<div class='city'><h5>" + cities[i] + "</h5></div>"
+    );
+  }
+  hover();
+}
+
+function hover() {
+  $(".city").on("click", function () {
+    console.log("test");
+  });
+}
